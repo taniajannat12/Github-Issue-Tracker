dab19b83cf75ad33836c9683e5620fd2f473dc11
taniajannat12 <taniajannat206@gmail.com>
2026-03-10 09:30:16 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById('sign').addEventListener('click',function(){
+    const name=document.getElementById('name');
+    const NAME=name.value
+    const pass=document.getElementById('pass');
+    const password=pass.value
+    if(NAME=='admin' && password=='admin123')
+    {
+        alert('success')
+        window.location.assign('/home.html')
+    }
+    else{
+        alert('failed')
+    }
+})
